4211103bd70fbc203e1b1a0630afe148e42ecfe8
Liam Stamper <136619788+liamstamper@users.noreply.github.com>
2024-09-17 01:50:11 -0500

    added some components

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,7 +1,16 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./login/LoginPage";
+import RegisterPage from "./login/RegisterPage";
+import HomePage from "./home/HomePage";
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/home" element={<HomePage />} />
+      </Routes>
+    </Router>
+  );
 }
