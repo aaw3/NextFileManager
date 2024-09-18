f5c2e4d86324e7640752fa001b62159e6964eb25
Liam Stamper <136619788+liamstamper@users.noreply.github.com>
2024-09-18 15:03:20 -0500

    removed register path

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import LoginPage from "./login/LoginPage";
-import RegisterPage from "./login/RegisterPage";
 import HomePage from "./home/HomePage";
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
         <Route path="/home" element={<HomePage />} />
       </Routes>
     </Router>
