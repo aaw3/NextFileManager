1392873d62f2be5f15b0a78f137a6f85e6b40be9
Liam Stamper <136619788+liamstamper@users.noreply.github.com>
2024-09-18 22:07:55 -0500

    installed ts and packages

--- my-project/src/home/HomePage.jsx
+++ my-project/src/home/HomePage.jsx
@@ -59,6 +59,12 @@ const HomePage = () => {
               <h2 className="text-xl font-bold dark:text-white">Recent</h2>
               <div>
                 <ButtonGroup />
+                <button
+                  type="button"
+                  class="ml-2 px-4 py-2 text-xs font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white"
+                >
+                  Filter
+                </button>
               </div>
             </div>
             <div className="overflow-auto">
