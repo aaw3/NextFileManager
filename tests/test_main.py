6e843ee8ac83c6d41194297d64ea4326b3c4b9da
Allen <63997543+aaw3@users.noreply.github.com>
2024-09-18 21:41:34 -0500

    Fix backend pytest

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,9 @@
 from fastapi.testclient import TestClient
-from app_fastapi import app
+from appfastapi import app
 
 client = TestClient(app)
 
 def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello, World!"}
+    assert response.json() == {"Hello": "World"}
