6160278b1c072cfc43acfe9866d6e4a344806abc
Liam Stamper <136619788+liamstamper@users.noreply.github.com>
2024-09-20 16:37:44 -0500

    fixed

--- my-project/src/components/home/ButtonGroup.tsx
+++ my-project/src/components/home/ButtonGroup.tsx
@@ -13,22 +13,22 @@ const ButtonGroup: React.FC<ButtonGroupProps> = ({
     <div className="inline-flex rounded-md shadow-sm" role="group">
       <button
         type="button"
-        className={`px-4 py-1 text-sm font-medium ${
+        className={`px-4 py-1 text-sm font-medium border border-gray-200 ${
           activeView === 2
-            ? " bg-gray-100 "
-            : "text-gray-900 bg-white border-gray-200"
-        } rounded-s-lg dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white`}
+            ? " bg-gray-200 dark:bg-gray-700"
+            : "dark:bg-gray-800"
+        } rounded-s-lg dark:border-gray-700 dark:text-white dark:hover:text-white dark:focus:text-white`}
         onClick={() => onChangeView(2)}
       >
         <i className="material-icons text-gray-600 dark:text-gray-400">list</i>
       </button>
       <button
         type="button"
-        className={`px-4 py-1 text-sm font-medium ${
+        className={`px-4 py-1 text-sm font-medium border border-gray-200 ${
           activeView === 1
-            ? " bg-gray-100 "
-            : "text-gray-900 bg-white border-gray-200"
-        } rounded-e-lg dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white`}
+            ? " bg-gray-200 dark:bg-gray-700"
+            : "dark:bg-gray-800"
+        } rounded-e-lg dark:border-gray-700 dark:text-white dark:hover:text-white dark:focus:text-white`}
         onClick={() => onChangeView(1)}
       >
         <i className="material-icons text-gray-600 dark:text-gray-400">
