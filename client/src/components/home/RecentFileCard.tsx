fcd4c850aeddf7f592dcbb1ca3062529fd8f77a5
Liam Stamper <136619788+liamstamper@users.noreply.github.com>
2024-09-24 21:05:57 -0500

    Context menu (#18)
    * frontend of context menu
    
    * removed arrow (wasnt spacing right)
    :
    
    * removed title and stock logo from login/register
    
    * removed extra comments
    
    * removed overflow auto
    
    * added filename prop and api requests
    
    * removed vite readme
    
    * context menu restyled and added to recentfilecard
    
    * rename added

--- client/src/components/home/RecentFileCard.tsx
+++ client/src/components/home/RecentFileCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ContextMenu from "./ContextMenu";
 
 interface RecentFileCardProps {
   file: {
@@ -9,8 +10,15 @@ interface RecentFileCardProps {
     imagepath: string;
     mime: string;
   };
+  isOpen: boolean;
+  toggleMenu: () => void;
 }
-const FileCard2: React.FC<RecentFileCardProps> = ({ file }) => {
+
+const RecentFileCard: React.FC<RecentFileCardProps> = ({
+  file,
+  isOpen,
+  toggleMenu,
+}) => {
   const formatDate = (epoch: string) => {
     const date = new Date(parseInt(epoch) * 1000);
     return date.toLocaleDateString("en-US", {
@@ -20,38 +28,47 @@ const FileCard2: React.FC<RecentFileCardProps> = ({ file }) => {
       day: "numeric",
     });
   };
+
   const removeFileExtension = (filename: string) => {
     return filename.replace(/\.[^/.]+$/, "");
   };
 
   return (
-    <Link
-      to="/"
-      className="bg-white rounded-lg shadow-md p-3 dark:bg-gray-800 flex flex-col hover:shadow-lg transition-shadow duration-200 ease-in-out"
-    >
-      <div className="w-full h-36 mb-2 bg-gray-200 dark:bg-gray-600 rounded-lg overflow-hidden">
-        <img
-          src={file.imagepath || "/path/to/placeholder-image.png"}
-          className="w-full h-full object-cover"
-        />
-      </div>
-      <div className="flex flex-row justify-between items-start">
-        <div className="flex-1">
+    <div className="relative">
+      <div className="bg-white border border-gray-300 dark:border-none rounded-lg p-3 dark:bg-gray-800 flex flex-col">
+        <div className="w-full h-36 mb-2 bg-gray-200 dark:bg-gray-600 rounded-lg overflow-hidden">
+          <img
+            src={file.imagepath || "/path/to/placeholder-image.png"}
+            className="w-full h-full object-cover"
+          />
+        </div>
+
+        <div className="flex justify-between items-center">
           <h3
             className="font-bold text-sm text-gray-900 dark:text-white truncate"
             title={file.name}
           >
-            {removeFileExtension(file.name)}{" "}
+            {removeFileExtension(file.name)}
           </h3>
+          <ContextMenu
+            fileName={file.name}
+            open="Open"
+            rename="Rename"
+            modify="Modify"
+            onDelete="Delete"
+            isOpen={isOpen}
+            toggleMenu={toggleMenu}
+          />
+        </div>
+
+        <div className="mt-2">
+          <span className="text-xs text-gray-500 dark:text-gray-400">
+            You modified • {formatDate(file.modified)}
+          </span>
         </div>
       </div>
-      <div className="mt-2">
-        <span className="text-xs text-gray-500 dark:text-gray-400">
-          You modified • {formatDate(file.modified)}
-        </span>
-      </div>
-    </Link>
+    </div>
   );
 };
 
-export default FileCard2;
+export default RecentFileCard;
