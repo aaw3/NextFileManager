e69763ba7b7bc1c1d14ed9b5691ce32e3f05597f
Allen <63997543+aaw3@users.noreply.github.com>
2024-10-08 21:58:59 -0500

    update CORS protocol

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://localhost:5173"],
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
