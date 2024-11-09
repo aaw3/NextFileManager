e4297daf1c77fefb2c99fc54cfd780105fdfc7b2
Liam Stamper <136619788+liamstamper@users.noreply.github.com>
2024-11-09 09:34:36 -0600

    added refreshdata to props and streamlined naming convention

--- client/src/components/home/SuggestedFilesGrid.tsx
+++ client/src/components/home/SuggestedFilesGrid.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import FileCard from "./SuggestedFileCard";
+import SuggestedFileCard from "./SuggestedFileCard";
 
 interface SuggestedFilesGridProps {
   files: {
@@ -9,13 +9,14 @@ interface SuggestedFilesGridProps {
     imagepath: string;
     mime_type: string;
   }[];
+  refreshData: () => void;
 }
 
-const SuggestedFilesGrid: React.FC<SuggestedFilesGridProps> = ({ files }) => {
+const SuggestedFilesGrid: React.FC<SuggestedFilesGridProps> = ({ files, refreshData }) => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 mb-8">
       {files.slice(0, 10).map((file, index) => (
-        <FileCard key={index} file={file} />
+        <SuggestedFileCard key={index} file={file} />
       ))}
     </div>
   );
